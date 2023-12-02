59a6537fde5f78870a83f64df2e8265f87e21b05
Jesica0 <100910537+Jesica0@users.noreply.github.com>
2023-12-01 21:55:08 -0500

    Actualizar config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -35,10 +35,10 @@ logging.basicConfig(
 class Config(object):
     WEBHOOK = os.environ.get("BOT_TOKEN", False)
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6814335289:AAGOBpcRetsv79P1Qep03s6_P54ikQYN7Uk")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID",14681595))
+    API_HASH = os.environ.get("a86730aab5c59953c424abb4396d32d5")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
@@ -63,9 +63,9 @@ class Config(object):
     # Set timeout for subprcess
     PROCESS_MAX_TIMEOUT = 3700
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
-    OWNER_ID = int(os.environ.get("OWNER_ID", "12356"))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002085553689))
+    OWNER_ID = int(os.environ.get("1759969205", "12356"))
+    BOT_USERNAME = os.environ.get("urlpv2_bot", "")
     ADL_BOT_RQ = {}
     AUTH_USERS = list({int(x)
                       for x in os.environ.get("AUTH_USERS", "0").split()})
